573344ed73a3140487fc6fc6f299d112e7046fdc
Tiffani <yho777@aucklanduni.ac.nz>
2025-03-01 17:15:23 +1300

    Add day6 exercise (tic tac toe with tutorial)

---
+++ day6/tic-tac-toe/tic-tac-toe-tutorial/src/app/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import './styles.css';
+
+import App from './App';
+
+export default function Home() {
+  return (
+    <div>
+      <App />
+    </div>
+  )
+}
